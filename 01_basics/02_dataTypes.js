a95ffccf693de8fbc5df0cc2055faa9be92c373b
Shubham Yadav <115391493+shubham26yadav@users.noreply.github.com>
2025-03-07 20:55:13 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+"use strict";  // treat all JS code as never version
+
+// alert(3+3) we are usig nodejs , not browser
+
+console.log(3 + 3);
+
+// number => 2 to poer 53
+// bigint
+// string => ""
+//undfined =>
+//symbol =>unique
+
+//object
+
+console.log(typeof undefined);
+console.log(typeof null);
+
+
