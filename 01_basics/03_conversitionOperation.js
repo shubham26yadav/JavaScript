d47363508d39e016b6581e683d4114ec173e29a6
Shubham Yadav <115391493+shubham26yadav@users.noreply.github.com>
2025-03-08 21:26:59 +0000

    Conversion of dataTypes

---
+++ 01_basics/03_conversitionOperation.js
@@ -0,0 +1,31 @@
+ let score = "33"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log((typeof valueInNumber));
+
+// console.log(valueInNumber);
+
+
+// notes
+// "33" => 33
+// "33abc" conversition => NaN(output)
+// true => 1; false => 0
+
+let isLoggedIn = "shubham"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1=> true; false => 0
+//"" => false
+// "shubham" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
