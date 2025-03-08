8aea859e6df9426faea82b0b01711adcddcd5b1d
Shubham Yadav <115391493+shubham26yadav@users.noreply.github.com>
2025-03-08 21:51:02 +0000

    Conversion of dataTypes

--- 01_basics/03_conversitionOperation.js
+++ 01_basics/03_conversitionOperation.js
@@ -21,11 +21,51 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 // 1=> true; false => 0
 //"" => false
-// "shubham" => true
+// "shubham" => true 
 
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ***************************** Operation *******************************
+
+let value  = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(5/2);
+// console.log(5%2);
+// console.log(2**3);
+
+let str1 = "Hello"
+let str2 = " Shubham!"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + "2");
+// console.log(1+2+"2");
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+++gameCounter;
+console.log(gameCounter);
+
+
+
+
+
+
+
+
+
 
